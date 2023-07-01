7b8953123279a3f34eae723a9796340ca1d70607
ashwini <ashwiniwagh690@gmail.com>
2023-07-01 18:30:01 +0530

    added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { OrdersModule } from './orders/orders.module';
+//import { OrdersModule_1 as OrdersModule } from "./orders/orders.module";
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        OrdersModule
+    ]
 })
 export class AppModule { }
